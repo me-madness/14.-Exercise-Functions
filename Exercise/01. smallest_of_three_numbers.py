b2fd96c1096aa51dacd59d64e7e2bca46d13e2e4
me-madness <58656549+me-madness@users.noreply.github.com>
2023-10-09 20:56:06 +0200

    01.11

--- Exercise/01. smallest_of_three_numbers.py
+++ Exercise/01. smallest_of_three_numbers.py
@@ -1,8 +1,9 @@
 n = int(input())
 empty_list = []
 # new_list = 0
-for number in range(n):
-    empty_list.append(number)
+for number in range(1, n + 1):
+    current_number = number
+    empty_list.append(current_number)
     
 # new_list.min(empty_list)
 print(empty_list)   
