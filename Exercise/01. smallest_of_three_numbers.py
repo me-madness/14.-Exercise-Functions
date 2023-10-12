c7ee3f6c6d85bf639fd5a33ff714145490d07ae1
me_madness <p_rangel@abv.bg>
2023-10-12 17:38:30 +0200

    01.

--- Exercise/01. smallest_of_three_numbers.py
+++ Exercise/01. smallest_of_three_numbers.py
@@ -1,9 +1,9 @@
-n = int(input())
-empty_list = []
-# new_list = 0
-for number in range(1, n + 1):
-    current_number = number
-    empty_list.append(current_number)
-    
-# new_list.min(empty_list)
-print(empty_list)   
+def smallest_number(some_numbers: list):
+   return min(some_numbers)
+
+
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())
+smallest_element = smallest_number([first_number, second_number, third_number])
+print(smallest_element)
