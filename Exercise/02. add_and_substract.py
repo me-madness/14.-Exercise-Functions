a52339d374ce9f8694eaef71fc56b90d16118b7c
me_madness <p_rangel@abv.bg>
2023-10-12 17:47:38 +0200

    02

---
+++ Exercise/02. add_and_substract.py
@@ -0,0 +1,16 @@
+def sum_numbers(first, second):
+    
+    
+    
+def substract(result, third):
+    
+    
+    
+def add_and_substract(first, second, third):
+    
+    
+    
+            
+first_number = int(input())
+second_number = int(input())
+third_number = int(input())    
